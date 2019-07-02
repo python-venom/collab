69d954e2b27037bbebf02517616786ac43ca140e
thedarklanter <52470798+thedarklanter@users.noreply.github.com>
2019-07-02 23:13:42 +0200

    Create nonamegame.py

---
+++ nonamegame.py
@@ -0,0 +1,15 @@
+print("wellcome to my world.")
+user1 = input("Name: ")
+print(user1 + "... a wonderful choice indeed")
+print("""in order to play this game you must know:
+	1. Every choice is represented by an number, when you make a choice, you
+	must type in the designated number (or, on occasion alphanumeric codes
+	 assigned to the choices.)
+	 """)
+print("You must now choose your weapons and skills.")
+print("""choose from the following types of weapons:
+	   1.Magic
+	   3.swords
+	   4.future technology
+	   5.parasitic
+	   """)
